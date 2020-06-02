9954415e23543964dced6f9f4545bdeb403384f3
Pit-Storm <33754750+Pit-Storm@users.noreply.github.com>
2020-06-02 21:50:04 +0200

    modifications to 03 and 00 in notebooks

--- notebooks/03_PM_tf1-a3c-research/a3c_research.py
+++ notebooks/03_PM_tf1-a3c-research/a3c_research.py
@@ -135,13 +135,13 @@ class MasterAgent():
       os.makedirs(save_dir)
 
     env = gym.make(self.game_name)
-    self.state_size = env.observation_space.shape[0]
+    self.state_size = env.observation_space.shape
     self.action_size = env.action_space.n
     self.opt = tf.train.AdamOptimizer(args.lr, use_locking=True)
     print(self.state_size, self.action_size)
 
     self.global_model = ActorCriticModel(self.state_size, self.action_size)  # global network
-    self.global_model(tf.convert_to_tensor(np.random.random((1, self.state_size)), dtype=tf.float32))
+    self.global_model(tf.convert_to_tensor(np.random.random((1, *self.state_size)), dtype=tf.float32))
 
   def train(self):
     if args.algorithm == 'random':
@@ -269,7 +269,8 @@ class Worker(threading.Thread):
             tf.convert_to_tensor(current_state[None, :],
                                  dtype=tf.float32))
         probs = tf.nn.softmax(logits)
-
+        tmp_probs = probs.numpy()[0]
+        # FIXME: Dimension of probs.numpy() for Pong-v0 has wrong dimension
         action = np.random.choice(self.action_size, p=probs.numpy()[0])
         new_state, reward, done, _ = self.env.step(action)
         if done:
