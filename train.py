8bee14b6a077aa8576eeb4beaa1785378881caeb
Pit-Storm <33754750+Pit-Storm@users.noreply.github.com>
2020-06-16 11:15:42 +0200

    removed tf-blogpost files

--- train.py
+++
@@ -1,6 +0,0 @@
-# Script for training automated with different parametersets
-
-# See: https://docs.python.org/3/library/subprocess.html#subprocess.run
-
-# TODO: Define parameters to train
-# TODO: define function to start training
